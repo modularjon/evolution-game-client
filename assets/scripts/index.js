3a039495d0cf9baa4d1fc2c0631b450c0df139d4
modularjon <modularjon@gmail.com>
2016-06-23 11:16:24 -0400

    Edit index.js to include auth events
    Add code to invoke auth events on document ready

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,5 +3,12 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+const authEvents = require('./auth/auth_events');
+
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// On document ready
+$(() => {
+  authEvents.addHandlers();
+});
