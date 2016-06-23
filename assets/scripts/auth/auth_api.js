7b4c2e1892d093b2cd806e1a17cf8aaf28c74bd4
modularjon <modularjon@gmail.com>
2016-06-23 11:05:13 -0400

    Edit auth_api, include user ajax requests
    Add code from handlebars training to deal with requests sent to the api

---
+++ assets/scripts/auth/auth_api.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const app = require('../app.js');
+
+const signUp = (data) => {
+  return $.ajax({
+    url: app.host + '/sign-up',
+    method: "POST",
+    data: data,
+  });
+};
+
+const signIn = (data) => {
+  return $.ajax({
+    url: app.host + '/sign-in',
+    method: "POST",
+    data: data,
+  });
+};
+
+const signOut = () => {
+  return $.ajax({
+    url: app.host + '/sign-out/' + app.user.id,
+    method: "DELETE",
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  });
+};
+
+const changePassword = (data) => {
+  return $.ajax({
+    url: app.host + '/change-password/' + app.user.id,
+    method: "PATCH",
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+    data: data,
+  });
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+};
