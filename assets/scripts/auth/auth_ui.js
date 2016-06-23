f433158b30caabe73510b0ead3ce7db784863805
modularjon <modularjon@gmail.com>
2016-06-23 11:11:58 -0400

    Add auth ui code to auth_ui.js
    Bring in code from handlebars lesson to alter ui after user actions

---
+++ assets/scripts/auth/auth_ui.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const app = require('../app.js');
+
+const success = (data) => {
+  if (data) {
+    console.log(data);
+  } else {
+    console.log('Success');
+  }
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+  $('.signed-out').hide();
+  $('.signed-in').show();
+  console.log(app.user);
+};
+
+const signOutSuccess = () => {
+  console.log('User signed out successfully');
+  $('.signed-in').hide();
+  $('.signed-out').show();
+  app.user = null;
+};
+
+module.exports = {
+  success,
+  failure,
+  signInSuccess,
+  signOutSuccess
+};
