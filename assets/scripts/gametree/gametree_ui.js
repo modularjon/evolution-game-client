8b3f4a03f6e4c3f4c3ed01a497494e6c55193a87
modularjon <modularjon@gmail.com>
2016-06-27 16:33:49 -0400

    Add anonymous function to get user score
    Displays with jQuery text method on class show-user-score

--- assets/scripts/gametree/gametree_ui.js
+++ assets/scripts/gametree/gametree_ui.js
@@ -4,6 +4,7 @@ const app = require('../app.js');
 
 const userScoreSuccess = (data) => {
   app.games = data.games;
+  $('.show-user-score').text(app.games.filter((obj) => { return obj.game_solved }).length);
 };
 
 const createGameSuccess = (data) => {
