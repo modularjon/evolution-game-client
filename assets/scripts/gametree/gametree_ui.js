cc8c7b3265d6c6f27acc674182d69eb37844ac23
modularjon <modularjon@gmail.com>
2016-06-28 16:10:21 -0400

    add codeblock to creategamesuccess
    assigns solution and tray arrays, use jquery to assign text values to tray and
    parent divs

--- assets/scripts/gametree/gametree_ui.js
+++ assets/scripts/gametree/gametree_ui.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('../app.js');
+const logic = require('./gametree_logic')
 
 const userScoreSuccess = (data) => {
   app.games = data.games;
@@ -9,6 +10,16 @@ const userScoreSuccess = (data) => {
 
 const createGameSuccess = (data) => {
   app.game = data.game;
+  logic.solution = app.game.child_taxons;
+  logic.tray = app.game.child_taxons.shuffled();
+  console.log(app.game.child_taxons);
+  console.log(logic.solution);
+  console.log(logic.tray);
+  $('.parent').text(app.game.parent_taxon.taxon_name);
+  $('.tray[data-id="0"]').text(logic.tray[0].taxon_name);
+  $('.tray[data-id="1"]').text(logic.tray[1].taxon_name);
+  $('.tray[data-id="2"]').text(logic.tray[2].taxon_name);
+  $('.tray[data-id="3"]').text(logic.tray[3].taxon_name);
 };
 
 module.exports = {
