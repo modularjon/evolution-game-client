20c3a071f3e5e90804dff51e1c911be5991c4860
modularjon <modularjon@gmail.com>
2016-06-28 19:27:11 -0400

    add selection variable

--- assets/scripts/gametree/gametree_logic.js
+++ assets/scripts/gametree/gametree_logic.js
@@ -6,16 +6,20 @@ let solution = [];
 
 let tray = [];
 
-let tree = [];
+let tree = ['','','',''];
+
+let selection;
+
+let gameSolved = false;
 
 Array.prototype.shuffled = function() {
   return this.map(function(n){ return [Math.random(), n] })
              .sort().map(function(n){ return n[1] });
 };
 
-const gameSolved = function(tray, tree) {
+const isGameSolved = function(tray, tree) {
   if (tray === tree) {
-    game_solved = true;
+    gameSolved = true;
   }
 };
 
@@ -23,5 +27,7 @@ module.exports = {
   solution,
   tray,
   tree,
+  selection,
   gameSolved,
+  isGameSolved,
 };
