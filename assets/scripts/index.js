0c4c1ea725260de4830a6b8900c974f07acd4a4a
modularjon <modularjon@gmail.com>
2016-06-28 15:48:10 -0400

    add missing semicolon

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,7 +4,7 @@
 // var example = require('./example');
 
 const authEvents = require('./auth/auth_events');
-const gameEvents = require('./gametree/gametree_events')
+const gameEvents = require('./gametree/gametree_events');
 
 // use require without a reference to ensure a file is bundled
 require('./example');
